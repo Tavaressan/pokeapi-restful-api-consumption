ea9b6dbb35f86e17524b690a21b479291be79b7b
Tavaressan <vitortctavares@gmail.com>
2025-07-24 20:12:04 -0300

    First commit

---
+++ src/main/java/me/dio/poke_project_api/PokeProjectApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.poke_project_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeProjectApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/poke_project_api/PokeProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.poke_project_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
